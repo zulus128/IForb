b52e411babfcd789786c7eee6328e985375bcc4e
Zul <Zulus@Zuls-Mac.local>
2013-04-22 23:33:32 -0700

    fi12399vtrr884f125554tz245601679777d6900115533scroll27r5502tt5t6f8899964344r

---
+++ IForb/LibViewController.h
@@ -0,0 +1,13 @@
+//
+//  LibViewController.h
+//  IForb
+//
+//  Created by Zul on 4/22/13.
+//  Copyright (c) 2013 вадим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LibViewController : UIViewController
+
+@end
---
+++ IForb/LibViewController.m
@@ -0,0 +1,38 @@
+//
+//  LibViewController.m
+//  IForb
+//
+//  Created by Zul on 4/22/13.
+//  Copyright (c) 2013 вадим. All rights reserved.
+//
+
+#import "LibViewController.h"
+
+@interface LibViewController ()
+
+@end
+
+@implementation LibViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
